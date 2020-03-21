adc992e5904502bd3b1a923a1f29c3bc73850a06
Steven Wright <steven@Stevens-MacBook-Pro.local>
2020-03-20 20:17:15 -0700

    Fix train, test

--- heightPrediction.py
+++ heightPrediction.py
@@ -21,28 +21,29 @@ momHeight = float(input("How tall is your mother (in inches)? "))
 gender = str(input("Are you M or F? "))
 height = float(0)
 
-#Put inputs into dictionary to create dataframe from inputs
 inputDict = {"Father":[dadHeight], "Mother":[momHeight], "Gender":[gender], "Height":[height]}
 inputdf = pd.DataFrame.from_dict(inputDict)
 
 #Create another dataframe from research data and append inputs
 df = pd.read_sql_query("select Father, Mother, Gender, Height from heightData", con=conn)
-df.dropna(how='any', inplace=True)
-df.append(inputdf)
 
-#Enconde Gender column
+#Drop null values
+df.dropna(how='any', inplace=True)
+inputdf.dropna(how='any', inplace=True)
+
+#Encode Gender columns
 le = preprocessing.LabelEncoder()
 df.Gender = le.fit_transform(df.Gender)
+inputdf.Gender = le.fit_transform(inputdf.Gender)
 
 #Predict height
 def prediction():
 
-    #Train test split
-    train, test = train_test_split(df, test_size=0.2)
-    X_train = train.drop(columns = ["Height"], axis = 1)
-    y_train = train["Height"]
-    X_test = test.drop(columns = [ "Height"], axis=1)
-    y_test = test["Height"]
+    #Train and test
+    X_train = df.drop(columns = ["Height"], axis = 1)
+    y_train = df["Height"]
+    X_test = inputdf.drop(columns = [ "Height"], axis=1)
+    y_test = inputdf["Height"]
 
     #Regression
     clf = linear_model.LinearRegression()
