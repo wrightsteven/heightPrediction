a85bf0f1edbc4e4413432e8634410c19ffe5fc50
Steven Wright <steven@Stevens-MacBook-Pro.local>
2020-03-20 19:55:29 -0700

    Read data from SQL

--- heightPrediction.py
+++ heightPrediction.py
@@ -6,47 +6,50 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
 import pymysql
 
-# setup connection
-host="host.rds.amazonaws.com" 
-port=3306
-dbname="sql-database"
+#Setup connection
+host="host" 
+port= 3306
+dbname="dbname"
 user="username"
 password="password"
 
-conn = pymysql.connect(host, user=user, passwd=password, port=port, db=dbname)
+conn = pymysql.connect(host, user=user, passwd=password, port=3306, db=dbname)
 
-#Get inputs
+#Get inputs and create dictionary
 dadHeight = float(input("How tall is your father (in inches)? "))
 momHeight = float(input("How tall is your mother (in inches)? "))
 gender = str(input("Are you M or F? "))
 height = float(0)
 
-inputDict = {"Father":dadHeight, "Mother": momHeight, "Gender":gender, "Height":height}
+#Put inputs into dictionary to create dataframe from inputs
+inputDict = {"Father":[dadHeight], "Mother":[momHeight], "Gender":[gender], "Height":[height]}
+inputdf = pd.DataFrame.from_dict(inputDict)
 
-#Create dataframe from research data
-df = pd.read_sql_table("heightData", con=conn, columns=["Father", "Mother", "Gender", "Height"])
-df.shuffle
+#Create another dataframe from research data and append inputs
+df = pd.read_sql_query("select Father, Mother, Gender, Height from heightData", con=conn)
 df.dropna(how='any', inplace=True)
+df.append(inputdf)
 
-# Add inputs to dataframe and encode
-df.append(inputDict)
+#Enconde Gender column
 le = preprocessing.LabelEncoder()
 df.Gender = le.fit_transform(df.Gender)
 
+#Predict height
 def prediction():
 
+    #Train test split
     train, test = train_test_split(df, test_size=0.2)
     X_train = train.drop(columns = ["Height"], axis = 1)
     y_train = train["Height"]
-
     X_test = test.drop(columns = [ "Height"], axis=1)
-    # y_test = test["Height"]
+    y_test = test["Height"]
 
+    #Regression
     clf = linear_model.LinearRegression()
     clf.fit(X_train, y_train)
 
-    #Predict height
+    #Prediction
     predict = clf.predict(X_test)
-    print("Expected height (inches): ", predict.tail(1))
+    print("Expected height (inches): ", predict[-1])
 
 prediction()
